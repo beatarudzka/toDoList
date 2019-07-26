70641938554143dfa296030269eb42a6518c9c1a
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-26 12:29:35 +0200

    creating classes

---
+++ script.js
@@ -0,0 +1,14 @@
+class Task {
+    constructor(text) {
+        this.text = text
+    }
+}
+
+class ToDoList {
+    constructor() {
+        this.tasks = ["umyj gary"]
+
+        this.render()
+    }
+
+}
