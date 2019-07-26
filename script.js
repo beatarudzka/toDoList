f881b9f7a5960edd482850299e396c4a3e0c40cb
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-26 13:29:01 +0200

    remove task functuon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class Task {
     constructor(text) {
         this.text = text
-        this.isFinished = false
+        this.isCompleted = false
     }
 }
 class ToDoList {
@@ -9,16 +9,45 @@ class ToDoList {
         this.tasks = []
         this.render()
     }
-
     render() {
+
         document.body.innerHTML = ''
+        this.promptFormForAddingTask()
+
         const ul = document.createElement('ul')
-        this.tasks.forEach(task => {
+        ul.className = 'todo-list'
+        this.tasks.forEach((task, taskIndex) => {
             const li = document.createElement('li')
+            const removeTaskButton = document.createElement('div')
+            const removeIcon = document.createTextNode("\u00D7")
+
+            li.classList.add('task')
+            removeTaskButton.className = 'delete-task-button'
+
+            li.addEventListener('click', (event) => {
+                event.target.classList.add('task--completed')
+                task.isCompleted = true
+            })
+
+            removeTaskButton.addEventListener('click', () => {
+                ul.removeChild(li)
+                this.tasks = this.tasks.slice(0, taskIndex).concat(this.tasks.slice(taskIndex + 1, this.tasks.length))
+                this.render()
+            })
+
+            removeTaskButton.appendChild(removeIcon)
             li.innerText = task.text
-            ul.appendChild.li
+            if (task.isCompleted) {
+                li.style.textDecorationColor = 'green'
+            }
+
+            li.appendChild(removeTaskButton)
+            ul.appendChild(li)
         })
+
+
         document.body.appendChild(ul)
+
     }
     addTaskToList(text) {
         if (text == '' || text == null) {
