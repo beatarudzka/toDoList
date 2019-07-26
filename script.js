559b8f239b3c5b7301eb4e9b75258062c63c5727
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-26 12:41:59 +0200

    creating render function

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ class Task {
 
 class ToDoList {
     constructor() {
-        this.tasks = ["umyj gary"]
+        this.tasks = ["umyj gary", 'zamieć', 'umyj lodówkę']
 
         this.render()
     }
     render() {
         document.body.innerHTML = ''
         const ul = document.createElement('ul')
+        this.tasks.forEach(task => {
+            const li = document.createElement('li')
+            li.innerText = task.text
+            ul.appendChild.li
+        })
         document.body.appendChild(ul)
     }
 
