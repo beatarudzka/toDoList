18fc33f782c02c594a7aa7596462f02737eb9ff2
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-27 10:18:23 +0200

    function searchTask

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ class ToDoList {
     }
 }
 
-const input = document.querySelector('input')
-const ul = document.querySelector('ul')
-const liElements = document.querySelectorAll('li')
-const searchTask = () => {
-    console.log('ok')
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+const liElements = document.querySelectorAll('li');
+
+const searchTask = (e) => {
+    const searchText = e.target.value.toLowerCase()
+    let tasks = [...liElements];
+    console.log(tasks);
+    tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText))
+    console.log(tasks);
+    ul.textContent = "";
+    tasks.forEach(li => ul.appendChild(li))
 }
 
 input.addEventListener('input', searchTask)
--- script1.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('input')
-const ul = document.querySelector('ul')
-const liElements = document.querySelectorAll('li')
-const searchTask = () => {
-    console.log('ok')
-}
-
-input.addEventListener('input', searchTask)
