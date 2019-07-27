58950b0076ca7c856d0ac6039d282cd6b899655c
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-27 12:07:24 +0200

    adding to localStorage

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+class Task {
+    constructor(text) {
+        this.text = text
+        this.completed = false
+    }
+}
 class ToDo {
     constructor() {
-        this.tasks = []
-
+        this.tasks = [] || JSON.parse(localStorage.getItem('toDo'))
         this.render()
     }
 
     addTask(text) {
         this.tasks.push(new Task(text))
-
+        this.dateInLocalStorage()
         this.render()
     }
-
 
 
     render() {
@@ -28,15 +32,16 @@ class ToDo {
 
             li.addEventListener('click', (e) => this.toggleComplete(task, li))
             button.addEventListener('click', (e) => this.taskDeleteClickHandler(e, taskIndex))
-            this.style(task, li)
-
-
 
             li.appendChild(button)
             ul.appendChild(li)
         })
-        document.body.appendChild(inputAddTask)
-        document.body.appendChild(buttonAddTask)
+
+
+
+        document.body.appendChild(ul)
+
+
 
     }
 
@@ -64,28 +69,44 @@ class ToDo {
 
         document.body.appendChild(inputAddTask)
         document.body.appendChild(buttonAddTask)
+
+        const inputFindTask = document.createElement('input')
+        const buttonFindTask = document.createElement('button')
+
+        buttonFindTask.innerText = 'Find Task'
+        buttonFindTask.addEventListener(
+            'click',
+            () => this.findTask(inputFindTask.value)
+        )
+        document.body.appendChild(inputFindTask)
+        document.body.appendChild(buttonFindTask)
+
+
     }
-}
-
-toggleComplete(task) {
-    task.completed ? (task.completed = false) : (task.completed = true)
-
-
-    this.render()
-
-}
-
-style(task, taskBox) {
-    task.completed ? (taskBox.style.textDecoration = "line-through") : (taskBox.style.textDecoration = "none")
-}
-
-}
-
-
-class Task {
-    constructor(text) {
-        this.text = text
+    findTask(input) {
+        const filtering = this.tasks.filter(task =>
+            task.text.toLowerCase().trim().includes(input.toLowerCase().trim()))
+        this.render(filtering)
     }
+
+
+
+    toggleComplete(task) {
+        task.completed ? (task.completed = false) : (task.completed = true)
+        this.dateInLocalStorage()
+        this.render()
+
+    }
+
+    style(task, taskBox) {
+        task.completed ? (taskBox.style.textDecoration = "line-through") : (taskBox.style.textDecoration = "none")
+    }
+
+    dateInLocalStorage() {
+        localStorage.setItem('toDo', `${JSON.stringify(this.tasks)}`)
+    }
+
 }
+
 
 const toDo1 = new ToDo()
