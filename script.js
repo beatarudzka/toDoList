29b3b1cdaf7f8484c78a1694727a408885db9bd7
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-27 13:14:46 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,150 +1,153 @@
-class Task {
-    constructor(text) {
-        this.text = text
-        this.completed = false
-    }
-}
-class ToDo {
-    constructor() {
-        this.tasks = [] || JSON.parse(localStorage.getItem('toDo'))
-        this.render()
-    }
+ class Task {
+     constructor(text) {
+         this.text = text
+         this.completed = false
+     }
+ }
+ class ToDo {
+     constructor() {
+         this.tasks = [] || JSON.parse(localStorage.getItem('toDo'))
+         this.render()
+     }
 
-    addTask(text) {
-        this.tasks.push(new Task(text))
-        this.dateInLocalStorage()
-        this.render()
-    }
-
-
-    render(arrayToFind) {
-        document.body.innerHTML = ''
-        const array = arrayToFind || this.tasks
-        this.makeUI()
-        const h1 = document.createElement('h1')
-
-        h1.innerText = 'to do list'
-        document.body.appendChild(h1)
+     addTask(text) {
+         this.tasks.push(new Task(text))
+         this.dateInLocalStorage()
+         this.render()
+     }
 
 
-        const ul = document.createElement('ul')
+     render(arrayToFind) {
+         document.body.innerHTML = ''
+         const array = arrayToFind || this.tasks
+         this.makeUI()
+         const h1 = document.createElement('h1')
 
-        array.forEach((task, taskIndex) => {
-            const li = document.createElement('li')
-            const button = document.createElement('button')
-            li.innerText = task.text
-            button.innerText = 'x'
-
-            li.addEventListener('click', (e) => this.toggleComplete(task, li))
-            button.addEventListener('click', (e) => this.taskDeleteClickHandler(e, taskIndex))
-
-            li.appendChild(button)
-            ul.appendChild(li)
-        })
+         h1.innerText = 'to do list'
+         document.body.appendChild(h1)
 
 
+         const ul = document.createElement('ul')
 
-        document.body.appendChild(ul)
+         array.forEach((task, taskIndex) => {
+             const li = document.createElement('li')
+             const button = document.createElement('button')
+             li.innerText = task.text
+             button.innerText = 'x'
+
+             li.addEventListener('click', (e) => this.toggleComplete(task, li))
+             button.addEventListener('click', (e) => this.taskDeleteClickHandler(e, taskIndex))
+
+             li.appendChild(button)
+             ul.appendChild(li)
+         })
 
 
 
-    }
-
-    taskClickHandler(task) {
-        alert("completed")
-    }
-
-    taskDeleteClickHandler(e, taskIndex) {
-        e.stopPropagation()
-        this.tasks = this.tasks.slice(0, taskIndex).concat(this.tasks.slice(taskIndex + 1))
-        this.render()
-    }
+         document.body.appendChild(ul)
 
 
 
-    makeUI() {
-        const inputAddTask = document.createElement('input')
-        const buttonAddTask = document.createElement('button')
-        buttonAddTask.innerText = 'Add task'
+     }
 
-        buttonAddTask.addEventListener(
-            'click',
-            () => this.addTask(inputAddTask.value)
-        )
+     taskClickHandler(task) {
+         alert("completed")
+     }
 
-        document.body.appendChild(inputAddTask)
-        document.body.appendChild(buttonAddTask)
-
-        const inputFindTask = document.createElement('input')
-        const buttonFindTask = document.createElement('button')
-
-        buttonFindTask.innerText = 'Find Task'
-        buttonFindTask.addEventListener(
-            'click',
-            () => this.findTask(inputFindTask.value)
-        )
-        const boxForButtons = document.createElement('div')
+     taskDeleteClickHandler(e, taskIndex) {
+         e.stopPropagation()
+         this.tasks = this.tasks.slice(0, taskIndex).concat(this.tasks.slice(taskIndex + 1))
+         this.render()
+     }
 
 
-        const buttonAllTasks = document.createElement('button')
-        buttonAllTasks.innerText = 'All'
-        buttonAllTasks.style.marginBottom = '10px'
-        buttonAllTasks.addEventListener('click', () => this.render())
 
-        const buttonDoneTasks = document.createElement('button')
-        buttonDoneTasks.innerText = 'Done'
-        buttonDoneTasks.style.marginBottom = '10px'
-        buttonDoneTasks.addEventListener('click', () => this.findDoneTask())
+     makeUI() {
+         const inputAddTask = document.createElement('input')
+         const buttonAddTask = document.createElement('button')
+         buttonAddTask.innerText = 'Add task'
 
+         buttonAddTask.addEventListener(
+             'click',
+             () => this.addTask(inputAddTask.value)
+         )
 
-        const buttonTasksToDo = document.createElement('button')
-        buttonTasksToDo.innerText = 'To Do'
-        buttonTasksToDo.style.marginBottom = '10px'
-        buttonTasksToDo.addEventListener('click', () => this.findToDoTask())
+         document.body.appendChild(inputAddTask)
+         document.body.appendChild(buttonAddTask)
 
-        boxForButtons.appendChild(buttonAllTasks)
-        boxForButtons.appendChild(buttonDoneTasks)
-        boxForButtons.appendChild(buttonTasksToDo)
-        document.body.appendChild(boxForButtons)
-        document.body.appendChild(inputAddTask)
-        document.body.appendChild(buttonAddTask)
-        document.body.appendChild(inputFindTask)
-        document.body.appendChild(buttonFindTask)
+         const inputFindTask = document.createElement('input')
+         const buttonFindTask = document.createElement('button')
 
-
-    }
-    findTask(input) {
-        const filtering = this.tasks.filter(task =>
-            task.text.toLowerCase().trim().includes(input.toLowerCase().trim()))
-        this.render(filtering)
-    }
-
-    findDoneTask() {
-        const doneTask = this.tasks.filter(task => task.completed === true)
-        this.render(doneTask)
-    }
-
-    findToDoTask() {
-        const toDoTask = this.tasks.filter(task => task.completed === false)
-        this.render(toDoTask)
-    }
-
-    toggleComplete(task) {
-        task.completed ? (task.completed = false) : (task.completed = true)
-        this.dateInLocalStorage()
-        this.render()
-
-    }
-
-    style(task, taskBox) {
-        task.completed ? (taskBox.style.textDecoration = "line-through") : (taskBox.style.textDecoration = "none")
-    }
-
-    dateInLocalStorage() {
-        localStorage.setItem('toDo', `${JSON.stringify(this.tasks)}`)
-    }
-
-}
+         buttonFindTask.innerText = 'Find Task'
+         buttonFindTask.addEventListener(
+             'click',
+             () => this.findTask(inputFindTask.value)
+         )
+         const boxForButtons = document.createElement('div')
 
 
-const toDo1 = new ToDo()
+         const buttonAllTasks = document.createElement('button')
+         buttonAllTasks.innerText = 'All'
+         buttonAllTasks.style.marginBottom = '10px'
+         buttonAllTasks.addEventListener('click', () => this.render())
+
+         const buttonDoneTasks = document.createElement('button')
+         buttonDoneTasks.innerText = 'Done'
+         buttonDoneTasks.style.marginBottom = '10px'
+         buttonDoneTasks.addEventListener('click', () => this.findDoneTask())
+
+
+         const buttonTasksToDo = document.createElement('button')
+         buttonTasksToDo.innerText = 'To Do'
+         buttonTasksToDo.style.marginBottom = '10px'
+         buttonTasksToDo.addEventListener('click', () => this.findToDoTask())
+
+         boxForButtons.appendChild(buttonAllTasks)
+         boxForButtons.appendChild(buttonDoneTasks)
+         boxForButtons.appendChild(buttonTasksToDo)
+         document.body.appendChild(boxForButtons)
+         document.body.appendChild(inputAddTask)
+         document.body.appendChild(buttonAddTask)
+         document.body.appendChild(inputFindTask)
+         document.body.appendChild(buttonFindTask)
+
+
+     }
+     findTask(input) {
+         const filtering = this.tasks.filter(task =>
+             task.text.toLowerCase().trim().includes(input.toLowerCase().trim()))
+         this.render(filtering)
+     }
+
+     findDoneTask() {
+         const doneTask = this.tasks.filter(task => task.completed === true)
+         this.render(doneTask)
+     }
+
+     findToDoTask() {
+         const toDoTask = this.tasks.filter(task => task.completed === false)
+         this.render(toDoTask)
+     }
+
+     toggleComplete(task) {
+         task.completed ? (task.completed = false) : (task.completed = true)
+
+         this.dateInLocalStorage()
+         this.render()
+
+     }
+
+     style(task, taskBox) {
+         task.completed ? (taskBox.style.textDecoration = "line-through") : (taskBox.style.textDecoration = "none")
+     }
+
+ }
+ dateInLocalStorage() {
+     localStorage.setItem('toDo', `${JSON.stringify(this.tasks)}`)
+ }
+
+ }
+ }
+
+
+ const toDo1 = new ToDo()
