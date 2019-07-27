c5751b8fa6e98065052be48aeff15351d1c0371e
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-27 09:21:43 +0200

    filtering is working

--- script.js
+++ script.js
@@ -5,66 +5,18 @@ class Task {
     }
 }
 class ToDoList {
-    constructor() {
+    constructor(selectedHtmlElement) {
         this.tasks = []
-        this.render()
-    }
-    render() {
-
-        document.body.innerHTML = ''
-        this.promptFormForAddingTask()
-
-        const ul = document.createElement('ul')
-        ul.className = 'todo-list'
-        this.tasks.forEach((task, taskIndex) => {
-            const li = document.createElement('li')
-            const removeTaskButton = document.createElement('div')
-            const removeIcon = document.createTextNode("\u00D7")
-
-            li.classList.add('task')
-            removeTaskButton.className = 'delete-task-button'
-
-            li.addEventListener('click', (event) => {
-                event.target.classList.add('task--completed')
-                task.isCompleted = true
-            })
-
-            removeTaskButton.addEventListener('click', () => {
-                ul.removeChild(li)
-                this.tasks = this.tasks.slice(0, taskIndex).concat(this.tasks.slice(taskIndex + 1, this.tasks.length))
-                this.render()
-            })
-
-            removeTaskButton.appendChild(removeIcon)
-            li.innerText = task.text
-            if (task.isCompleted) {
-                li.style.textDecorationColor = 'green'
-            }
-
-            li.appendChild(removeTaskButton)
-            ul.appendChild(li)
-        })
-
-
-        document.body.appendChild(ul)
-
-    }
-    addTaskToList(text) {
-        if (text == '' || text == null) {
-            alert("Add new task")
-        } else {
-            this.tasks.push(new Task(text))
-            console.log(this.tasks)
-        }
-        this.render()
-    }
-    createFormForAddingTask() {
-        const input = document.createElement('input')
-        const button = document.createElement('button')
-        button.innerText = 'Create new task'
-        button.addEventListener('click', () => this.addTaskToList(input.value))
-        document.body.appendChild(input)
-        document.body.appendChild(button)
+        this.selectedHtmlElement = selectedHtmlElement || document.body
 
     }
 }
+
+const input = document.querySelector('input')
+const ul = document.querySelector('ul')
+const liElements = document.querySelectorAll('li')
+const searchTask = () => {
+    console.log('ok')
+}
+
+input.addEventListener('input', searchTask)
---
+++ script1.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input')
+const ul = document.querySelector('ul')
+const liElements = document.querySelectorAll('li')
+const searchTask = () => {
+    console.log('ok')
+}
+
+input.addEventListener('input', searchTask)
